7c3df218790ead7e0730585c034629202958cf20
knottedbrain <yilkalargawworkneh@gmail.com>
2024-06-23 22:40:25 +0000

    more modifications to aliases

--- headless/.bashrc.d/aliases.sh
+++ headless/.bashrc.d/aliases.sh
@@ -8,13 +8,31 @@ if [ -x "$(command -v eza)" ]; then
 	alias tree='eza --tree'
 fi
 
-alias la='ls -ah'
-alias ll='ls -lh'
-alias lal='ls -alh'
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    # alias ls='ls --color=auto'
+    alias dir='dir --color=auto'
+    alias vdir='vdir --color=auto'
+
+    #alias grep='grep --color=auto'
+    #alias fgrep='fgrep --color=auto'
+    #alias egrep='egrep --color=auto'
+fi
+
+# some more ls aliases
+alias ll='ls -l'
+alias la='ls -A'
+
+# alias l='ls -CF'
+# alias la='ls -ah'
+# alias ll='ls -lh'
+# alias lal='ls -alh'
 # alias la='ls -a'
 # alias ll='ls -l'
 # alias lal='ls -al'
-alias lah='ls -lah'
+# alias lah='ls -lah'
+
 alias feh='feh -g 640x480  -dZ.'
 
 label="com.gitlab.uppercat.tlbx"
